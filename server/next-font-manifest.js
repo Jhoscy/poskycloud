bd61846dc0bd463befd4669424c1979591e03c88
Gionata <g.stante@reply.it>
2023-07-30 21:29:00 +0200

    Updates

--- server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/utente/Desktop/Jonny/Projects/poskycloud/posky-sea/app/layout":["static/media/e2fcd21359e56fab-s.p.otf"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
