d2bec73e81cf802d6afd91c70a2a7a78c81aa9f5
Gionata <g.stante@reply.it>
2023-07-31 17:00:56 +0200

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
-import Head from 'next/head'
 
 const dogica = localFont({
   src: '../public/fonts/dogica/dogicapixel.otf',
@@ -9,27 +8,59 @@ const dogica = localFont({
 })
 
 export const metadata: Metadata = {
+  metadataBase: new URL(`https://jhoscy.github.io/${process.env.NEXT_PUBLIC_BASE_PATH}`),
   title: 'Posky Cloud',
-  description: 'Navighiamo',
+  description: 'A Posky Cloud space over the web',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  creator: 'Gionata Stante',
+  publisher: 'Gionata Stante',
+  manifest: `${process.env.NEXT_PUBLIC_BASE_PATH }/favicon/site.webmanifest`,
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
   icons: {
     icon: [{
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/favicon/favicon-32x32.png',
+      url: `${process.env.NEXT_PUBLIC_BASE_PATH }/favicon/favicon-32x32.png`,
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      url: '/favicon/favicon-16x16.png',
+      url: `${process.env.NEXT_PUBLIC_BASE_PATH }/favicon/favicon-16x16.png`,
     },
     {
       rel: 'apple-touch-icon',
       sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
+      url: `${process.env.NEXT_PUBLIC_BASE_PATH }/favicon/apple-touch-icon.png`,
     },
     ]
+  },
+  openGraph: {
+    title: 'Posky Cloud',
+    description: 'A Posky Cloud space over the web',
+    url: `https://jhoscy.github.io/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+    siteName: 'poskycloud',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/full.gif`,
+        width: 800,
+        height: 600,
+        alt: 'Sea',
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/full.gif`,
+        width: 1800,
+        height: 1600,
+        alt: 'Sea',
+      },
+    ],
+    locale: 'it_IT',
+    type: 'website',
   },
 }
 
@@ -40,13 +71,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-      </Head>
       <body className={dogica.className}>
         {children}
         <div id='react-modal'></div>
