149d8e2931433df5d9db8e49eb9e967b6c376962
Gionata <g.stante@reply.it>
2023-07-31 11:51:36 +0200

    Restored

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withImages = require("next-images");
 
-module.exports = withImages({
+module.exports = {
   // Your Next.js configuration options here
   output: 'export',
   basePath: "/poskycloud",
   assetPrefix: "/poskycloud/",
-});
+};
